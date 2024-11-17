fcb42bb060dea177450a2b4c814cf3c41fe69025
Patchara Al-umaree <patcharaalumaree@gmail.com>
2024-11-17 22:35:30 +0700

    up

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector("form").addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    // เก็บข้อมูลจากฟอร์ม
-    var formData = {
-        name: document.getElementById('name').value,
-        date: document.getElementById('date').value,
-        time: document.getElementById('time').value
-    };
+        // เก็บข้อมูลจากฟอร์ม
+        var formData = {
+            name: document.getElementById('name').value,
+            date: document.getElementById('date').value,
+            time: document.getElementById('time').value
+        };
 
-    // ส่งข้อมูลไปยัง Google Apps Script Web App
-    fetch('https://script.google.com/macros/s/AKfycbyeCz1Iw146wVhZBNM1JZY6fvKPQqcL6VYExbQLpWJ5q1ydlrD5KwqXuNIkAa9_v0C0/exec', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        alert(data.message);  // แสดงข้อความตอบกลับจาก Google Apps Script
-    })
-    .catch((error) => {
-        console.error('Error:', error);
+        // ส่งข้อมูลไปยัง Google Apps Script Web App
+        fetch('https://script.google.com/macros/s/AKfycbyeCz1Iw146wVhZBNM1JZY6fvKPQqcL6VYExbQLpWJ5q1ydlrD5KwqXuNIkAa9_v0C0/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)  // ส่งข้อมูลในรูปแบบ JSON
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            alert(data.message);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     });
 });
